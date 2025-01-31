5226a66ae2f6832a34175ac50903a111257186e8
Salutah <salutahmoha@gmail.com>
2025-01-31 12:28:46 +0300

    Building testimonials section with carousel

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,34 +1,34 @@
-// import React from "react";
-// import "./Testimonials.css";
-// import testimonials from "../Data/testimonialData";
-// import Carousel from "react-elastic-carousel";
+import React from "react";
+import "./Testimonials.css";
+import testimonials from "../Data/testimonialData";
+import Carousel from "react-elastic-carousel";
 
-// function Testimonials() {
-//   return (
-//     <section id="testimonials">
-//       <h3>Testimonials from Alumni</h3>
-//       <div className="testimonial-container">
-//         <Carousel
-//           className="carousel"
-//           enableAutoPlay={true}
-//           autoPlaySpeed={3000}
-//         >
-//           {testimonials.map((testimonial, index) => (
-//             <div key={index} className="testimonial-content">
-//               <div className="user-details">
-//                 <img src={testimonial.image} alt="" />
-//                 <h5>{testimonial.name}</h5>
-//               </div>
-//               <div className="user-feedback">
-//                 <h4>{testimonial.title}</h4>
-//                 <p>{testimonial.quote}</p>
-//               </div>
-//             </div>
-//           ))}
-//         </Carousel>
-//       </div>
-//     </section>
-//   );
-// }
+function Testimonials() {
+  return (
+    <section id="testimonials">
+      <h3>Testimonials from Alumni</h3>
+      <div className="testimonial-container">
+        <Carousel
+          className="carousel"
+          enableAutoPlay={true}
+          autoPlaySpeed={3000}
+        >
+          {testimonials.map((testimonial, index) => (
+            <div key={index} className="testimonial-content">
+              <div className="user-details">
+                <img src={testimonial.image} alt="" />
+                <h5>{testimonial.name}</h5>
+              </div>
+              <div className="user-feedback">
+                <h4>{testimonial.title}</h4>
+                <p>{testimonial.quote}</p>
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </section>
+  );
+}
 
-// export default Testimonials;
+export default Testimonials;
