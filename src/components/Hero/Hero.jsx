8bad3c042c55d6206b5177e7417a23e8c01ddd71
Salutah <salutahmoha@gmail.com>
2025-01-31 12:26:05 +0300

    Building Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,20 @@
-// import React from "react";
-// import "./Hero.css";
+import React from "react";
+import "./Hero.css";
 
-// function Hero() {
-//   return (
-//     <section id="hero">
-//       <div className="hero-container">
-//         <h3>Somakodi Tech Club</h3>
-//         <p>
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-//           nam at numquam ullam porro, vero eos accusantium, labore perferendis
-//           earum omnis? Delectus laudantium hic temporibus reiciendis est quis
-//           eligendi vero.
-//         </p>
-//       </div>
-//     </section>
-//   );
-// }
+function Hero() {
+  return (
+    <section id="hero">
+      <div className="hero-container">
+        <h3>Somakodi Tech Club</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          nam at numquam ullam porro, vero eos accusantium, labore perferendis
+          earum omnis? Delectus laudantium hic temporibus reiciendis est quis
+          eligendi vero.
+        </p>
+      </div>
+    </section>
+  );
+}
 
-// export default Hero;
+export default Hero;
