99034912a1641e2e660a75760695f82cb4e2c541
Salutah <salutahmoha@gmail.com>
2025-01-31 12:30:03 +0300

    Building the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,53 +1,53 @@
-// import React from "react";
-// import "./Footer.css";
-// import { MdOutlineEmail } from "react-icons/md";
-// import { FaPhone } from "react-icons/fa";
-// import { FaLocationDot } from "react-icons/fa6";
-// import { FaFacebookF } from "react-icons/fa";
-// import { FaInstagram } from "react-icons/fa";
-// import { FaYoutube } from "react-icons/fa";
+import React from "react";
+import "./Footer.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 
-// function Footer() {
-//   return (
-//     <section id="footer">
-//       <div className="footer-container">
-//         <div className="footer-info">
-//           <div className="footer-details">
-//             <h4>Food Express</h4>
-//             <p className="icon"><MdOutlineEmail />jacob#gmail.com</p>
-//             <p className="icon"><FaPhone />0734567890</p>
-//             <p className="icon"><FaLocationDot />CBD Harambee Avenue</p>
-//           </div>
-//           <div className="footer-details">
-//             <h4>Our Menu</h4>
-//             <p>BreakFast</p>
-//             <p>Lunch</p>
-//             <p>Dinner</p>
-//           </div>
-//           <div className="footer-details">
-//             <h4>Useful Links</h4>
-//             <p>Services</p>
-//             <p>Help & Support</p>
-//             <p>Terms & Conditions</p>
-//           </div>
-//           <div className="footer-details">
-//             <h4>Social Media</h4>
-//             <p className="icon"><FaFacebookF />Facebook</p>
-//             <p className="icon"><FaInstagram />Instagram</p>
-//             <p className="icon"> <FaYoutube />Youtube</p>
-//           </div>
-//         </div>
-//         <div className="horizontal-line">
+function Footer() {
+  return (
+    <section id="footer">
+      <div className="footer-container">
+        <div className="footer-info">
+          <div className="footer-details">
+            <h4>Food Express</h4>
+            <p className="icon"><MdOutlineEmail />jacob#gmail.com</p>
+            <p className="icon"><FaPhone />0734567890</p>
+            <p className="icon"><FaLocationDot />CBD Harambee Avenue</p>
+          </div>
+          <div className="footer-details">
+            <h4>Our Menu</h4>
+            <p>BreakFast</p>
+            <p>Lunch</p>
+            <p>Dinner</p>
+          </div>
+          <div className="footer-details">
+            <h4>Useful Links</h4>
+            <p>Services</p>
+            <p>Help & Support</p>
+            <p>Terms & Conditions</p>
+          </div>
+          <div className="footer-details">
+            <h4>Social Media</h4>
+            <p className="icon"><FaFacebookF />Facebook</p>
+            <p className="icon"><FaInstagram />Instagram</p>
+            <p className="icon"> <FaYoutube />Youtube</p>
+          </div>
+        </div>
+        <div className="horizontal-line">
 
-//         </div>
+        </div>
 
-//         <div className="copyright">
-//             <p>&copy;Copyright 2025, Designed with ❤️ by Salad</p>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
+        <div className="copyright">
+            <p>&copy;Copyright 2025, Designed with ❤️ by Salad</p>
+        </div>
+      </div>
+    </section>
+  );
+}
 
-// export default Footer;
+export default Footer;
