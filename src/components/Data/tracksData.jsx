30aaf813567b601e941136819b32365511f79c3b
Salutah <salutahmoha@gmail.com>
2025-01-31 12:34:48 +0300

    Building tracksData file for storing data for tracks section

--- src/components/Data/tracksData.jsx
+++ src/components/Data/tracksData.jsx
@@ -1,41 +1,41 @@
-// import cloud from "../../assets/cloud.png";
-// import cyber from "../../assets/cyber.png";
-// import power from "../../assets/power.jpeg";
-// import web from "../../assets/web.png";
-// import uxui from "../../assets/uxui.png";
-// import mobile from "../../assets/mobile.jpeg";
+import cloud from "../../assets/cloud.png";
+import cyber from "../../assets/cyber.png";
+import power from "../../assets/power.jpeg";
+import web from "../../assets/web.png";
+import uxui from "../../assets/uxui.png";
+import mobile from "../../assets/mobile.jpeg";
 
-// const tracksData = [
-//   {
-//     image: cloud,
-//     text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
-//     title: "Cloud Engineer",
-//   },
-//   {
-//     image: cyber,
-//     text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
-//     title: "Cyber Security",
-//   },
-//   {
-//     image: web,
-//     text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
-//     title: "Web developer",
-//   },
-//   {
-//     image: power,
-//     text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
-//     title: "Electrical Engineer",
-//   },
-//   {
-//     image: mobile,
-//     text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
-//     title: "Mobile Developer",
-//   },
-//   {
-//     image: uxui,
-//     text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
-//     title: "uxui Engineer",
-//   },
-// ];
+const tracksData = [
+  {
+    image: cloud,
+    text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
+    title: "Cloud Engineer",
+  },
+  {
+    image: cyber,
+    text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
+    title: "Cyber Security",
+  },
+  {
+    image: web,
+    text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
+    title: "Web developer",
+  },
+  {
+    image: power,
+    text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
+    title: "Electrical Engineer",
+  },
+  {
+    image: mobile,
+    text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
+    title: "Mobile Developer",
+  },
+  {
+    image: uxui,
+    text: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.Had denoting properly jointure you occasion directly raillery",
+    title: "uxui Engineer",
+  },
+];
 
-// export default tracksData;
+export default tracksData;
