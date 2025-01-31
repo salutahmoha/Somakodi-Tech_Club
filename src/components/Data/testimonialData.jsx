6f35dd0395cf3db823449abc7fff17943b945ad4
Salutah <salutahmoha@gmail.com>
2025-01-31 12:33:54 +0300

    Building testimonialData file for storing data for testimonial-section

--- src/components/Data/testimonialData.jsx
+++ src/components/Data/testimonialData.jsx
@@ -1,50 +1,50 @@
-// import test3 from "../../assets/test3.jpeg";
-// import test2 from "../../assets/test2.jpeg";
-// import test1 from "../../assets/test1.jpeg";
+import test3 from "../../assets/test3.jpeg";
+import test2 from "../../assets/test2.jpeg";
+import test1 from "../../assets/test1.jpeg";
 
-// const testimonials = [
-//   {
-//     image: test3,
-//     name: "Faith ",
-//     title: "Software Engineer",
-//     quote:
-//       "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
-//   },
-//   {
-//     image: test2,
-//     name: "Brian",
-//     title: "Data Scientist",
-//     quote:
-//       "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
-//   },
-//   {
-//     image: test1,
-//     name: "Jacob ",
-//     title: "Quality Assurance Engineer",
-//     quote:
-//       "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
-//   },
-//   {
-//     image: test3,
-//     name: "Faith ",
-//     title: "Software Engineer",
-//     quote:
-//       "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
-//   },
-//   {
-//     image: test2,
-//     name: "Brian",
-//     title: "Data Scientist",
-//     quote:
-//       "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
-//   },
-//   {
-//     image: test1,
-//     name: "Jacob ",
-//     title: "Quality Assurance Engineer",
-//     quote:
-//       "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
-//   },
-// ];
+const testimonials = [
+  {
+    image: test3,
+    name: "Faith ",
+    title: "Software Engineer",
+    quote:
+      "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
+  },
+  {
+    image: test2,
+    name: "Brian",
+    title: "Data Scientist",
+    quote:
+      "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
+  },
+  {
+    image: test1,
+    name: "Jacob ",
+    title: "Quality Assurance Engineer",
+    quote:
+      "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
+  },
+  {
+    image: test3,
+    name: "Faith ",
+    title: "Software Engineer",
+    quote:
+      "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
+  },
+  {
+    image: test2,
+    name: "Brian",
+    title: "Data Scientist",
+    quote:
+      "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
+  },
+  {
+    image: test1,
+    name: "Jacob ",
+    title: "Quality Assurance Engineer",
+    quote:
+      "Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold.",
+  },
+];
 
-// export default testimonials;
+export default testimonials;
