3631466d0ed18ef6de674eaae3c2889a5e073dc4
Salutah <salutahmoha@gmail.com>
2025-01-31 12:24:35 +0300

    Building navbar file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,47 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-// import "./Navbar.css";
-// import logo from "../../assets/somalogo.png";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/somalogo.png";
 
-// function Navbar() {
-//   return (
-//     <section id="navbar">
-//       <div className="navbar-container">
-//         <div className="logo">
-//           <img src={logo} alt="" />
-//         </div>
-//         <div className="nav-items">
-//           <ul className="nav-lists">
-//             <li>
-//               <NavLink to="/" className="nav-list">
-//                 Home
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink to="/Leadership" className="nav-list">
-//                 Leadership
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink to="/Tracks" className="nav-list">
-//                 Tracks
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink to="/Events" className="nav-list">
-//                 Events
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink to="/Contact" className="nav-list">
-//                 Contact Us
-//               </NavLink>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
+function Navbar() {
+  return (
+    <section id="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="nav-items">
+          <ul className="nav-lists">
+            <li>
+              <NavLink to="/" className="nav-list">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Leadership" className="nav-list">
+                Leadership
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Tracks" className="nav-list">
+                Tracks
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Events" className="nav-list">
+                Events
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact" className="nav-list">
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
 
-// export default Navbar;
+export default Navbar;
